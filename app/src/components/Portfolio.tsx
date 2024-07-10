178356cbaa9c9c7b59bce18737dc1df81e40b894
Lilach E <lilachelayza@gmail.com>
2024-07-10 17:39:16 +0300

    create the Project component

--- app/src/components/Portfolio.tsx
+++ app/src/components/Portfolio.tsx
@@ -1,8 +1,35 @@
+import Project from "./Project";
+import styles from "./Portfolio.module.scss";
+
 export default function Portfolio() {
   return (
     <div className="green">
       <div className="content">
         <h1>Portfolio</h1>
+        <div className={styles.projectsWheel}>
+          <Project title="Project 1" imageUrl="https://picsum.photos/200/200">
+            <p>React</p>
+            <p>Node.js</p>
+            <p>MongoDB</p>
+          </Project>
+          <Project title="Project 2" imageUrl="https://picsum.photos/200/200">
+            <p>React</p>
+            <p>Node.js</p>
+            <p>MySQL</p>
+          </Project>
+          <Project title="Project 3" imageUrl="https://picsum.photos/200/200">
+            <p>React-Native</p>
+            <p>Node.js</p>
+            <p>redux</p>
+            <p>MongoDB</p>
+          </Project>
+          <Project title="Project 4" imageUrl="https://picsum.photos/200/200">
+            <p>React-Native</p>
+            <p>Node.js</p>
+            <p>react-query</p>
+            <p>MySQL</p>
+          </Project>
+        </div>
       </div>
     </div>
   );
---
+++ app/src/components/Project.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import styles from "./Project.module.scss";
+
+type ProjectProps = {
+  title: string;
+  imageUrl: string;
+  children: ReactNode;
+};
+export default function Project({ title, imageUrl, children }: ProjectProps) {
+  return (
+    <div className={styles.projectContainer}>
+      <h2>{title}</h2>
+      <img src={imageUrl} alt="random picture" />
+      {children}
+    </div>
+  );
+}
