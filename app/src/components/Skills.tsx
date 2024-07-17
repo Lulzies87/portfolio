3a0dfa6f50a086f1203de4e414dc287c8bdcc5e8
Lilach E <lilachelayza@gmail.com>
2024-07-17 13:49:20 +0300

    minor fixes to skills scrollbar

--- app/src/components/Skills.tsx
+++ app/src/components/Skills.tsx
@@ -30,54 +30,56 @@ export default function Skills() {
 
   return (
     <div className="green">
-      <div className={`${styles.skillsContainer} section content`}>
-        <button className={styles.scrollButton} onClick={scrollLeft}>
-          {"<"}
-        </button>
-        <div id="skills" className={styles.skillsContent} ref={skillsRef}>
-          <h1>Skills</h1>
-          <ul className={styles.skillsList}>
-            <li>
-              <img src={htmlIcon} alt="html5 icon" />
-            </li>
-            <li>
-              <img src={javascriptIcon} alt="javascript icon" />
-            </li>
-            <li>
-              <img src={typescriptIcon} alt="typescript icon" />
-            </li>
-            <li>
-              <img src={cssIcon} alt="css icon" />
-            </li>
-            <li>
-              <img src={reactIcon} alt="react icon" />
-            </li>
-            <li>
-              <img src={nodejsIcon} alt="nodejs icon" />
-            </li>
-            <li>
-              <img src={expressIcon} alt="express icon" />
-            </li>
-            <li>
-              <img src={mongodbIcon} alt="mongodb icon" />
-            </li>
-            <li>
-              <img src={mysqlIcon} alt="mysql icon" />
-            </li>
-            <li>
-              <img src={gitIcon} alt="git icon" />
-            </li>
-            <li>
-              <img src={githubIcon} alt="github icon" />
-            </li>
-            <li>
-              <img src={npmIcon} alt="npm icon" />
-            </li>
-          </ul>
+      <div className="section content">
+        <h1>Skills</h1>
+        <div className={styles.skillsContainer}>
+          <button className={styles.scrollButton} onClick={scrollLeft}>
+            {"<"}
+          </button>
+          <div id="skills" className={styles.skillsContent} ref={skillsRef}>
+            <ul className={styles.skillsList}>
+              <li>
+                <img src={htmlIcon} alt="html5 icon" />
+              </li>
+              <li>
+                <img src={javascriptIcon} alt="javascript icon" />
+              </li>
+              <li>
+                <img src={typescriptIcon} alt="typescript icon" />
+              </li>
+              <li>
+                <img src={cssIcon} alt="css icon" />
+              </li>
+              <li>
+                <img src={reactIcon} alt="react icon" />
+              </li>
+              <li>
+                <img src={nodejsIcon} alt="nodejs icon" />
+              </li>
+              <li>
+                <img src={expressIcon} alt="express icon" />
+              </li>
+              <li>
+                <img src={mongodbIcon} alt="mongodb icon" />
+              </li>
+              <li>
+                <img src={mysqlIcon} alt="mysql icon" />
+              </li>
+              <li>
+                <img src={gitIcon} alt="git icon" />
+              </li>
+              <li>
+                <img src={githubIcon} alt="github icon" />
+              </li>
+              <li>
+                <img src={npmIcon} alt="npm icon" />
+              </li>
+            </ul>
+          </div>
+          <button className={styles.scrollButton} onClick={scrollRight}>
+            {">"}
+          </button>
         </div>
-        <button className={styles.scrollButton} onClick={scrollRight}>
-          {">"}
-        </button>
       </div>
     </div>
   );
