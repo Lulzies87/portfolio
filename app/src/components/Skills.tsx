821c9876fae0fd527b7e33d9fcb59070d3934228
Lilach E <lilachelayza@gmail.com>
2024-07-29 16:21:08 +0300

    fix skills scrolling issue on mobile

--- app/src/components/Skills.tsx
+++ app/src/components/Skills.tsx
@@ -1,8 +1,20 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./Skills.module.scss";
 
 export default function Skills() {
   const skillsRef = useRef<HTMLDivElement>(null);
+
+  const updateScroll = () => {
+    if (skillsRef.current) {
+      const container = skillsRef.current;
+      const itemWidth = container.firstElementChild?.clientWidth || 1800;
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+      if (container.scrollLeft + itemWidth > maxScrollLeft) {
+        container.scrollLeft = maxScrollLeft;
+      }
+    }
+  };
 
   const scrollLeft = () => {
     if (skillsRef.current) {
@@ -14,59 +26,124 @@ export default function Skills() {
 
   const scrollRight = () => {
     if (skillsRef.current) {
-      const itemWidth =
-        skillsRef.current.firstElementChild?.clientWidth || 1800;
-      skillsRef.current.scrollBy({ left: itemWidth, behavior: "smooth" });
+      const container = skillsRef.current;
+      const itemWidth = container.firstElementChild?.clientWidth || 1800;
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+      if (container.scrollLeft + itemWidth >= maxScrollLeft) {
+        container.scrollLeft = maxScrollLeft;
+      } else {
+        container.scrollBy({ left: itemWidth, behavior: "smooth" });
+      }
     }
   };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScroll);
+    return () => window.removeEventListener("resize", updateScroll);
+  }, []);
 
   return (
     <div className="background-light">
       <div id="skills" className="section content">
         <h1>Skills</h1>
         <div className={styles.skillsContainer}>
-          <button className={`${styles.scrollButton} ${styles.scrollLeft}`} onClick={scrollLeft}></button>
+          <button
+            className={`${styles.scrollButton} ${styles.scrollLeft}`}
+            onClick={scrollLeft}
+          ></button>
           <div className={styles.skillsContent} ref={skillsRef}>
             <ul className={styles.skillsList}>
               <li>
-                <img src="/images/icons/html.png" draggable="false" alt="html5 icon" />
+                <img
+                  src="/images/icons/html.png"
+                  draggable="false"
+                  alt="html5 icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/javascript.png" draggable="false" alt="javascript icon" />
+                <img
+                  src="/images/icons/javascript.png"
+                  draggable="false"
+                  alt="javascript icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/typescript.png" draggable="false" alt="typescript icon" />
+                <img
+                  src="/images/icons/typescript.png"
+                  draggable="false"
+                  alt="typescript icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/css.png" draggable="false" alt="css icon" />
+                <img
+                  src="/images/icons/css.png"
+                  draggable="false"
+                  alt="css icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/react.png" draggable="false" alt="react icon" />
+                <img
+                  src="/images/icons/react.png"
+                  draggable="false"
+                  alt="react icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/nodejs.png" draggable="false" alt="nodejs icon" />
+                <img
+                  src="/images/icons/nodejs.png"
+                  draggable="false"
+                  alt="nodejs icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/express.png" draggable="false" alt="express icon" />
+                <img
+                  src="/images/icons/express.png"
+                  draggable="false"
+                  alt="express icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/mongodb.png" draggable="false" alt="mongodb icon" />
+                <img
+                  src="/images/icons/mongodb.png"
+                  draggable="false"
+                  alt="mongodb icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/mysql.png" draggable="false" alt="mysql icon" />
+                <img
+                  src="/images/icons/mysql.png"
+                  draggable="false"
+                  alt="mysql icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/git.png" draggable="false" alt="git icon" />
+                <img
+                  src="/images/icons/git.png"
+                  draggable="false"
+                  alt="git icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/github.png" draggable="false" alt="github icon" />
+                <img
+                  src="/images/icons/github.png"
+                  draggable="false"
+                  alt="github icon"
+                />
               </li>
               <li>
-                <img src="/images/icons/npm.png" draggable="false" alt="npm icon" />
+                <img
+                  src="/images/icons/npm.png"
+                  draggable="false"
+                  alt="npm icon"
+                />
               </li>
             </ul>
           </div>
-          <button className={`${styles.scrollButton} ${styles.scrollRight}`} onClick={scrollRight}></button>
+          <button
+            className={`${styles.scrollButton} ${styles.scrollRight}`}
+            onClick={scrollRight}
+          ></button>
         </div>
       </div>
     </div>
