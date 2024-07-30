5f5221271342b627c4c4a733860f307659c166b0
Lilach E <lilachelayza@gmail.com>
2024-07-30 18:15:28 +0300

    infinite scrolling

--- app/src/components/Skills.tsx
+++ app/src/components/Skills.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./Skills.module.scss";
 
 export default function Skills() {
   const skillsRef = useRef<HTMLDivElement>(null);
-  const [scrollDirection, setScrollDirection] = useState<"right" | "left">(
-    "right"
-  );
 
   useEffect(() => {
     let intervalId: number;
@@ -14,33 +11,19 @@ export default function Skills() {
       intervalId = window.setInterval(() => {
         if (skillsRef.current) {
           const container = skillsRef.current;
-          const maxScrollLeft = container.scrollWidth - container.clientWidth;
+          container.scrollBy({ left: 1, behavior: "instant" });
 
-          if (scrollDirection === "right") {
-            if (container.scrollLeft >= maxScrollLeft) {
-              setScrollDirection("left");
-            } else {
-              container.scrollBy({ left: 1, behavior: "instant" });
-            }
-          } else {
-            if (container.scrollLeft <= 0) {
-              setScrollDirection("right");
-            } else {
-              container.scrollBy({ left: -1, behavior: "instant" });
-            }
+          if (container.scrollLeft >= container.scrollWidth / 2) {
+            container.scrollLeft = 0;
           }
         }
       }, 20);
     };
 
-    const stopAutoScroll = () => clearInterval(intervalId);
-
     startAutoScroll();
 
-    return () => {
-      stopAutoScroll();
-    };
-  }, [scrollDirection]);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="background-light">
@@ -49,6 +32,90 @@ export default function Skills() {
         <div className={styles.skillsContainer}>
           <div className={styles.skillsContent} ref={skillsRef}>
             <ul className={styles.skillsList}>
+              <li>
+                <img
+                  src="/images/icons/html.png"
+                  draggable="false"
+                  alt="html5 icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/javascript.png"
+                  draggable="false"
+                  alt="javascript icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/typescript.png"
+                  draggable="false"
+                  alt="typescript icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/css.png"
+                  draggable="false"
+                  alt="css icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/react.png"
+                  draggable="false"
+                  alt="react icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/nodejs.png"
+                  draggable="false"
+                  alt="nodejs icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/express.png"
+                  draggable="false"
+                  alt="express icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/mongodb.png"
+                  draggable="false"
+                  alt="mongodb icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/mysql.png"
+                  draggable="false"
+                  alt="mysql icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/git.png"
+                  draggable="false"
+                  alt="git icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/github.png"
+                  draggable="false"
+                  alt="github icon"
+                />
+              </li>
+              <li>
+                <img
+                  src="/images/icons/npm.png"
+                  draggable="false"
+                  alt="npm icon"
+                />
+              </li>
               <li>
                 <img
                   src="/images/icons/html.png"
