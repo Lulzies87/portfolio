3e6e776f7f63de42fd364c9ac9f76c5fdede352e
Lilach E <lilachelayza@gmail.com>
2025-02-24 12:01:39 +0200

    new about me paragraph

--- app/src/components/About/About.tsx
+++ app/src/components/About/About.tsx
@@ -69,12 +69,12 @@ export default function About() {
           <p
             className={styles.aboutContainer__innerWrap__textContainer__content}
           >
-            {`I'm a mother, wife, coder, and gamer.
-                Motherhood inspired me to strive to be the best role model for my daughter, which led me to pursue a new career path as a Full Stack Developer.
-                I successfully completed an intensive year-long Full Stack course where my curiosity and passion for coding grew immensely.
-                I developed this site to showcase my abilities and it is regularly updated with my latest content.
-                As a lifelong learner, I am driven to continuously explore and apply cutting-edge technologies in web development. I thrive on solving complex problems, optimizing workflows, and delivering efficient solutions.
-                Life, for me, is about setting goals and working to achieve them, becoming a little better with each milestone I reach.`}
+            {`Full Stack Development isn’t just my profession - it’s my passion.
+            With a strong foundation in both front-end and back-end technologies, I love bringing every component together in perfect harmony, ensuring seamless communication between them.
+            Problem-solving is my strength, workflow optimization is second nature, and building high-performance applications is what drives me.
+            I am committed to continuous learning, staying ahead of emerging technologies, and refining my skills to push the boundaries of web development.
+            I'm looking for a dynamic work environment where I can be challenged, innovate, and grow as a developer.
+            This site was developed to showcase my journey and projects that reflect my dedication to clean code, innovation, and seamless user experiences.`}
           </p>
         </div>
       </div>
